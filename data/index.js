e319dcfa42176b1f4f1eec9ad78c3b75827882a9
Josh Peachey <joshpeachey24@gmail.com>
2020-10-23 16:59:56 +0100

    Started working on northcoders/username task

--- data/index.js
+++ data/index.js
@@ -2,6 +2,18 @@ const http = require('http');
 const fs = require('fs');
 const { sendNorthcoders, helloApi } = require('./controllers');
 
+
+
+
+
+const ncObj = require('./JSONdata/northcoders.json');
+const usernames = [];
+
+for(const person of ncObj) {
+    usernames.push(person.username);
+}
+
+
 const server = http.createServer(function (req, res) {
     const {url, method} = req;
 
@@ -16,8 +28,16 @@ const server = http.createServer(function (req, res) {
         helloApi(req, res);
         };
     }
+
+    for(let username of usernames) {
+        if (url === `/api/northcoders/${username}`) {
+            if(method === 'GET') {
+        }
+    }
+    }
 });
    
+
 
 server.listen(8080);
 
